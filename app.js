79217171a6f51b2002ce21dafcf08c09e85c140c
amir-kohjani <amir.kohjani@outlook.com>
2022-11-04 21:19:58 +0330

    add express Json middleware

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const mongoose = require('mongoose');
 const connection = require('./DAL/Connection');
 const app = express();
 require('dotenv').config();
+const productRouter = require('./routers/productsRouter')
+
+
+//----------------Middlewares----------------------
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //--------------------Connection to DataBase-----------------------------
 connection();
+
+
 //------------------Running Server------------------ -------
 app.listen(process.env.PORT || 3000, () => {
     console.log(`listening on port ${process.env.PORT || 3000}`);
