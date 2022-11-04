6985d8e64841b4054cc8d4cbf1e69dc363587b8a
amir-kohjani <amir.kohjani@outlook.com>
2022-11-04 20:58:36 +0330

    fix product  model

--- models/productModel/productModel.js
+++ models/productModel/productModel.js
@@ -1,53 +1,68 @@
 const mongoose = require('mongoose');
 const ColorModel = require('./colorModel');
 const productSchema = new mongoose.Schema({
-    id: { type: 'string', required: true },
-    category: [{type:String,required:true}],
-    // title: { type: 'string', required: true },
-    // price: {
-    //     type: 'number',
-    //     required: true,
-    // },
-    // discount: {
-    //     type: 'number',
-    //     required: false
-    // },
-    // priceWithDiscount: {
-    //     type: "number",
-    //     required: function () {
-    //         return this.discount;
-    //     },
-    //     default: function () {
-    //         return this.discount ? this.price - ((this.price * this.discount) / 100) : null;
-    //     },
+    id: {
+        type: 'string',
+        required: true
+    },
+    title: {
+        type: 'string',
+        required: true,
+        minLength: 10,
+        maxLength: 100
+    },
+    category: [{
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 50
+    }],
+    price: {
+        type: 'number',
+        required: true,
+    },
+    discount: {
+        type: 'number',
+        required: false,
+        minLength: 10,
+        maxLength: 100
+    },
+    priceWithDiscount: {
+        type: "number",
+        required: function () {
+            return this.discount;
+        },
+        default: function () {
+            return this.discount ? this.price - ((this.price * this.discount) / 100) : null;
+        },
 
-    // },
-    // img: {
-    //     type: "string",
-    //     required: true,
-    // },
-    // desc: {
-    //     type: "string",
-    //     required: true,
-    // },
-    // brand: {
-    //     type: "string",
-    //     required: true,
-    // },
-    // colors: { type: [ColorModel], required: true },
-    // sizes: {
-    //     type: "string",
-    //     required: true,
-    //     enum: ["XS", "S", "M", "L", "XL"]
-    // },
+    },
+    img: {
+        type: "string",
+        required: true,
+    },
+    desc: {
+        type: "string",
+        required: true,
+    },
+    brand: {
+        type: "string",
+        required: true,
+    },
+    colors: { type: [ColorModel], required: true },
+    sizes: {
+        type: "string",
+        required: true,
+        enum: ["XS", "S", "M", "L", "XL"]
+    },
 
-    // stock: {
-    //     type: "boolean",
-    //     default: true,
-    // },
-    // created_at: { type: Date },
-    // updated_at: { type: Date, default: Date.now },
-    // updated: { type: Date, default: Date.now }
+    stock: {
+        type: "boolean",
+        default: true,
+    },
+    created_at: { type: Date },
+    updated_at: { type: Date, default: Date.now },
+    updated: { type: Date, default: Date.now }
 
 })
 // productSchema.methods.setDiscount = function setDiscount() {
