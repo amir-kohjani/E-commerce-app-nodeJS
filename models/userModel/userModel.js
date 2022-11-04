e50764cab793afc7e4230cc4a41d753a36d1e0b9
amir-kohjani <amir.kohjani@outlook.com>
2022-11-04 20:58:06 +0330

    create user model

---
+++ models/userModel/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+    name: {
+        type: 'string',
+        required: true,
+        minLength: 3,
+        maxLength: 100,
+    },
+    email: {
+        type: 'string',
+        required: true,
+        minLength: 3,
+        maxLength: 320,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    },
+    phone:{
+
+  type: 'string',
+        required: true,
+        minLength: 11,
+        maxLength: 11,
+        match: /09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/
+    },
+    passwords: {
+        type: 'string',
+        required: true,
+        length:60,
+    },
+    
+})
