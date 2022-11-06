10a2fc771a7b41eb4dad121777265cedc279f69b
amir-kohjani <amir.kohjani@outlook.com>
2022-11-06 13:02:38 +0330

    add some route

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const productController = require('../controllers/productController');
+
+router.get('/',(req, res) => {});
+router.get('/byCategory',productController.getProductsByCategory);
+router.get('/byId',productController.getProductById);
+router.get('/ByTitle',productController.getProductsByTitle);
+
+router.post('/',productController.create);
 
 
-router.get('/',(req, res) => {
- 
-})
+
 
 
 module.exports =router;
