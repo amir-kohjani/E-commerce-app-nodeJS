264626b362fbc065b92d070200b30bbea838c79c
amir-kohjani <amir.kohjani@outlook.com>
2022-12-02 16:43:55 +0330

    fix DATABASE URL address

--- DAL/Connection.js
+++ DAL/Connection.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const connection = ()=>{
-    mongoose.connect(`mongodb://localhost:27017/${ process.env.DB_NAME}`, { useNewUrlParser: true })
+const connection = () => {
+    mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, authSource: "admin" })
     mongoose.connection.once('open', () => {
-        console.log(`mongoose connected to "${ process.env.DB_NAME}" ! `);
+        console.log(`mongoose connected to "${process.env.DB_NAME}" ! `);
     })
 }
 module.exports = connection;
