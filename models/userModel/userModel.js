f3e1fcd2443827c39ecef949451226e580b5f1c9
amir-kohjani <amir.kohjani@outlook.com>
2022-12-02 16:42:58 +0330

    fix something in user model

--- models/userModel/userModel.js
+++ models/userModel/userModel.js
@@ -1,33 +1,63 @@
 const mongoose = require('mongoose');
+const AddressSchema = require('./addressSchema');
+const shortUniqId = require("short-unique-id")
+const uid = new shortUniqId({
+    dictionary: 'number',
+    length: 6
+});
 const userSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 6,
+        maxLength: 6,
+        default: () => uid()
+    },
     name: {
         type: 'string',
-        required: true,
+        minLength: 3,
+        maxLength: 100,
+        default: 'کاربر عزیز'
+    },
+    lastName: {
+        type: 'string',
         minLength: 3,
         maxLength: 100,
     },
-    email: {
-        type: 'string',
-        required: true,
-        minLength: 3,
-        maxLength: 320,
-        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
-    },
-    phone:{
+    phone: {
 
-  type: 'string',
+        type: 'string',
         required: true,
         minLength: 11,
         maxLength: 11,
-        match: /09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/
+        match: /09(1[0-9]|3[0-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}/
+    },
+    email: {
+        type: 'string',
+        minLength: 3,
+        maxLength: 320,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+
     },
     passwords: {
         type: 'string',
-        required: true,
-        length:60,
+        length: 60,
+
     },
+    addressList: {
+        type: [AddressSchema],
+        default: []
+
+    },
+    token: {
+        type: "string"
+    },
+    tokens: {
+        type: [String]
+    }
 
 })
 
-const UserModel = mongoose.model('UserModel',userSchema);
+const UserModel = mongoose.model('Users', userSchema);
 module.exports = UserModel;
