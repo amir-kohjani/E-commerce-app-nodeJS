00d9ad7c6fd4aa72312849d267ac48a66b4a1ea6
amir-kohjani <amir.kohjani@outlook.com>
2022-12-02 16:43:21 +0330

    add redis package and more ...

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const connection = require('./DAL/Connection');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
+
 const productRouter = require('./routers/productsRouter')
+const authRouter = require('./routers/authRouter')
+const userRouter = require('./routers/userRouter')
+const cartRouter = require('./routers/cartRouter');
 
 
 //----------------Middlewares----------------------
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -15,6 +21,9 @@ connection();
 
 //--------------------------Routers----------------------------------------
 app.use('/product',productRouter)
+app.use('/auth',authRouter);
+app.use('/user',userRouter);
+app.use('/cart',cartRouter);
 //------------------Running Server------------------ -------
 app.listen(process.env.PORT || 3000, () => {
     console.log(`listening on port ${process.env.PORT || 3000}`);
